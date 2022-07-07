e8f8827e7692c5a78b601944b9af3dd1c812b80e
spratham <pratham919singh@gmail.com>
2022-07-07 19:41:19 +0530

    Final commit

---
+++ specs/calculator.spec.js
@@ -0,0 +1,12 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import Calculator from '@/Calculator.vue'
+import ElementUI from 'element-ui' 
+
+const localVue = createLocalVue()
+localVue.use(ElementUI)
+
+describe('Test Calculator', () => {
+  const wrapper = mount(Calculator, { localVue } )
+  test('is a Vue instance', () => {
+  })
+})
